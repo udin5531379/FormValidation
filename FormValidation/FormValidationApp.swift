29bebd7d0047ca5dad52c97669de98dcbc385536
System Administrator <root@MacBook-Pro.local>
2021-02-14 20:38:08 -0600

    First Commit

---
+++ FormValidation/FormValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormValidationApp.swift
+//  FormValidation
+//
+//  Created by Udin Rajkarnikar on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct FormValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
